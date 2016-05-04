3656948cd53ab83b8fc3e89aa7255f357ab99628
#manosenelcódigo <peligrosamente@gmail.com>
2016-05-03 23:24:17 -0300

    archivo.php

---
+++ archivo.php
@@ -0,0 +1,35 @@
+<?php
+
+ /*
+    - trabajar de forma dinámica 
+    - trabajar con trazabilidad
+    - vamos a reutilizar recursos
+    - implementar integración de herramientas
+ */
+ //ciclo while
+ 
+    /*
+    mientras
+        suceda ésto
+            haz ésto otro
+    */
+    $i=0;
+    while( $i<10 )
+    {
+        if($i%2==0 and $i!=0)//imprimiendo los números pares
+        {
+           echo $i; 
+        }
+        
+        
+        $i++;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
