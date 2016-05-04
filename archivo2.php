4b6caf279f59c550124f1bb58f7e9b26731e845b
#manosenelcódigo <peligrosamente@gmail.com>
2016-05-03 23:24:34 -0300

    archivo2.php

---
+++ archivo2.php
@@ -0,0 +1,15 @@
+<?php
+/*
+    for
+    
+    hacer esto
+        desde el inicio al final de la condición
+*/
+    for($i=0 ; $i<10 ; $i++ )
+    {
+         if($i%3==0 and $i!=0)//imprimiendo los números pares
+        {
+           echo $i; 
+        }
+        
+    }
